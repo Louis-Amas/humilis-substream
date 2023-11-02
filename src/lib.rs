45a95ca98869a90bcd469db42b5c40e8298c9324
Louis-Amas <amaslouis@protonmail.com>
2023-11-02 19:15:35 +0100

    feat(prototype): tests substreams to listen all tx traces

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,154 @@
 mod pb;
 
-use pb::acme::call::v1::{BlockCall, Call as MyCall, StorageChange};
+use std::collections::HashMap;
+
+use pb::acme::call::v1::{BlockHeader, TransactionTrace, Call, StorageChange, BalanceChange, TransactionReceipt, Log};
+use prost::Message;
 use substreams::Hex;
 use substreams_ethereum::pb::eth::{self};
+use substreams_ethereum::pb::eth::v2 as substream_lib;
 
-#[substreams::handlers::map]
-fn map_block(block: eth::v2::Block) -> Result<BlockCall, substreams::errors::Error> {
-    let mut calls: Vec<MyCall> = Vec::new();
+fn get_storage_changes(keccak_preimages: HashMap<String, String>, storage_changes: Vec<substream_lib::StorageChange>) -> Vec<StorageChange> {
+    let mut changes: Vec<StorageChange> = Vec::new();
+    for change in storage_changes.into_iter() {
+        let key_as_hex = Hex::encode(&change.key);
+        let result = keccak_preimages.get(&key_as_hex);
 
-    for tx in block.transactions() {
-        for call in tx.calls.to_vec() {
-            let mut changes: Vec<StorageChange> = Vec::new();
-            for change in call.storage_changes {
-                let key_as_hex = Hex::encode(change.key);
-                let result = call.keccak_preimages.get(&key_as_hex);
-
-                let mut preimg = "".to_string();
-                if result.is_some() {
-                    preimg = result.unwrap().clone();
-                }
-
-                changes.push(StorageChange { 
-                    address: Hex::encode(change.address),
-                    key: key_as_hex, 
-                    preimg, 
-                    old_value: Hex::encode(change.old_value), 
-                    new_value: Hex::encode(change.new_value), 
-                    ordinal: change.ordinal,
-                });
-            }
-            calls.push(MyCall {
-                address: Hex::encode(call.address),
-                storage_changes: changes,
-            });
+        let mut preimg = "".to_string();
+        if result.is_some() {
+            preimg = result.unwrap().clone();
         }
+
+        changes.push(StorageChange { 
+            // address: change.address,
+            // key: change.key,
+            preimg: preimg.encode_to_vec(),
+            // old_value: change.old_value,
+            new_value: change.new_value,
+        });
     }
 
-    Ok(BlockCall {
-        calls,
+    changes
+}
+
+fn get_balance_change(raw_balance_changes: Vec<substream_lib::BalanceChange>) -> Vec<BalanceChange> {
+    let mut balance_changes = Vec::new();
+
+    for change in raw_balance_changes.into_iter() {
+        balance_changes.push(BalanceChange { 
+            // address: change.address, 
+            // old_value: change.old_value.map(|val| val.bytes), 
+            new_value: change.new_value.map(|val| val.bytes), 
+            // reason: change.reason,
+        })
+    }
+
+    balance_changes
+}
+
+fn get_calls(raw_calls: Vec<substream_lib::Call>) -> Vec<Call> {
+    let mut calls: Vec<Call> = Vec::new();
+
+    for call in raw_calls.into_iter() {
+        calls.push(Call { 
+            // index: call.index, 
+            // parent_index: call.parent_index, 
+            // call_type: call.call_type, 
+            // caller: call.caller, 
+            address: call.address, 
+            // value: call.value.map(|val| val.bytes), 
+            // gas_limit: call.gas_limit, 
+            // gas_consumed: call.gas_consumed, 
+            // return_data: call.return_data, 
+            // input: call.input, 
+            // exectued_code: call.executed_code, 
+            // suicide: call.suicide, 
+            storage_changes: get_storage_changes(call.keccak_preimages, call.storage_changes), 
+            // balance_changes: get_balance_change(call.balance_changes) 
+        })
+    }
+
+    calls
+}
+
+fn get_logs(raw_logs: Vec<substream_lib::Log>) -> Vec<Log> {
+    let mut logs: Vec<Log> = Vec::new();
+    for log in raw_logs.into_iter() {
+        logs.push(Log { 
+            // address: log.address, 
+            topics: log.topics, 
+            data: log.data, 
+            // index: log.index, 
+            // block_index: log.block_index
+        })
+    }
+
+    logs
+}
+
+fn get_transactions_receipt(raw_receipt: Option<substream_lib::TransactionReceipt>) -> Option<TransactionReceipt> {
+    if raw_receipt.is_none() {
+        return None;
+    }
+
+    let receipt = raw_receipt.unwrap();
+    Some(TransactionReceipt {
+        // state_root: receipt.state_root,
+        // cumulative_gas_used: receipt.cumulative_gas_used,
+        // logs_bloom: receipt.logs_bloom,
+        logs: get_logs(receipt.logs)
+    }) 
+}
+
+fn get_transaction_traces(raw_transactions: Vec<substream_lib::TransactionTrace>) -> Vec<TransactionTrace> {
+    let mut traces: Vec<TransactionTrace> = Vec::new();
+    for tx in raw_transactions.into_iter() {
+        traces.push(TransactionTrace { 
+            to: tx.to,
+            // nonce: tx.nonce,
+            // gas_price: tx.gas_price.map(|val| val.bytes),
+            // gas_limit: tx.gas_limit,
+            // input: tx.input,
+            // gas_used: tx.gas_used,
+            // r#type: tx.r#type,
+            // max_fee_per_gas: tx.max_fee_per_gas.map(|val| val.bytes),
+            // max_priority_fee_per_gas: tx.max_priority_fee_per_gas.map(|val| val.bytes),
+            // index: tx.index,
+            status: tx.status,
+            hash: tx.hash,
+            from: tx.from,
+            return_data: tx.return_data,
+            receipt: get_transactions_receipt(tx.receipt),
+            calls: get_calls(tx.calls),
+        })
+    }
+
+    traces
+}
+
+#[substreams::handlers::map]
+fn map_block(block: eth::v2::Block) -> Result<BlockHeader, substreams::errors::Error> {
+    let timestamp = block.timestamp_seconds();
+    let block_header = block.header.unwrap();
+    Ok(BlockHeader {
+        parent_hash: block_header.parent_hash,
+        // uncle_hash: block_header.uncle_hash,
+        // coinbase: block_header.coinbase,
+        // state_root: block_header.state_root,
+        // transaction_root: block_header.transactions_root,
+        logs_bloom: block_header.logs_bloom,
+        // difficulty: block_header.difficulty.map(|val| val.bytes),
+        // total_difficulty: block_header.total_difficulty.map(|val| val.bytes),
+        // number: block_header.number,
+        // gas_limit: block_header.gas_limit,
+        // gas_used: block_header.gas_used,
+        timestamp,
+        // extra_data: block_header.extra_data,
+        // mix_hash: block_header.mix_hash,
+        // nonce: block_header.nonce,
+        hash: block_header.hash,
+        // base_fee_per_gas: block_header.base_fee_per_gas.map(|val| val.bytes),
+        // withdrawals_root: block_header.withdrawals_root,
+        transactions: get_transaction_traces(block.transaction_traces),
     })
 }
--- src/pb/acme.call.v1.rs
+++ src/pb/acme.call.v1.rs
@@ -1,32 +1,267 @@
 // @generated
+/// bytes address = 1;
+/// bytes key = 2;
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct StorageChange {
-    #[prost(string, tag="1")]
-    pub address: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
-    pub key: ::prost::alloc::string::String,
-    #[prost(string, tag="3")]
-    pub preimg: ::prost::alloc::string::String,
-    #[prost(string, tag="4")]
-    pub old_value: ::prost::alloc::string::String,
-    #[prost(string, tag="5")]
-    pub new_value: ::prost::alloc::string::String,
-    #[prost(uint64, tag="6")]
-    pub ordinal: u64,
+    #[prost(bytes="vec", tag="3")]
+    pub preimg: ::prost::alloc::vec::Vec<u8>,
+    /// bytes old_value = 4;
+    #[prost(bytes="vec", tag="5")]
+    pub new_value: ::prost::alloc::vec::Vec<u8>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct BalanceChange {
+    /// bytes address = 1;
+    /// optional bytes old_value = 2;
+    #[prost(bytes="vec", optional, tag="3")]
+    pub new_value: ::core::option::Option<::prost::alloc::vec::Vec<u8>>,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Call {
-    #[prost(string, tag="1")]
-    pub address: ::prost::alloc::string::String,
-    #[prost(message, repeated, tag="2")]
+    /// uint32 index = 1;
+    /// uint32 parent_index = 2;
+    /// CallType call_type = 3;
+    /// bytes caller = 4;
+    ///
+    /// optional bytes value = 6;
+    /// uint64 gas_limit = 7;
+    /// uint64 gas_consumed = 8;
+    ///
+    /// bytes return_data = 9;
+    /// bytes input = 10;
+    ///
+    /// bool exectued_code = 11;
+    /// bool suicide = 12;
+    #[prost(bytes="vec", tag="5")]
+    pub address: ::prost::alloc::vec::Vec<u8>,
+    /// repeated BalanceChange balance_changes = 14;
+    #[prost(message, repeated, tag="13")]
     pub storage_changes: ::prost::alloc::vec::Vec<StorageChange>,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct BlockCall {
-    #[prost(message, repeated, tag="1")]
+pub struct Log {
+    /// bytes address = 1;
+    #[prost(bytes="vec", repeated, tag="2")]
+    pub topics: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
+    /// uint32 index = 4;
+    #[prost(bytes="vec", tag="3")]
+    pub data: ::prost::alloc::vec::Vec<u8>,
+}
+/// bytes state_root = 1;
+/// uint64 cumulative_gas_used = 2;
+/// bytes logs_bloom = 3;
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TransactionReceipt {
+    #[prost(message, repeated, tag="4")]
+    pub logs: ::prost::alloc::vec::Vec<Log>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TransactionTrace {
+    #[prost(bytes="vec", tag="1")]
+    pub to: ::prost::alloc::vec::Vec<u8>,
+    /// uint64 nonce = 2;
+    /// optional bytes gas_price = 3;
+    /// uint64 gas_limit = 4;
+    /// bytes input = 5;
+    /// uint64 gas_used = 6;
+    /// int32 type = 7;
+    ///
+    /// optional bytes max_fee_per_gas = 8;
+    /// optional bytes max_priority_fee_per_gas = 9;
+    ///
+    /// uint32 index = 10;
+    #[prost(enumeration="TransactionTraceStatus", tag="11")]
+    pub status: i32,
+    #[prost(bytes="vec", tag="12")]
+    pub hash: ::prost::alloc::vec::Vec<u8>,
+    #[prost(bytes="vec", tag="13")]
+    pub from: ::prost::alloc::vec::Vec<u8>,
+    #[prost(bytes="vec", tag="14")]
+    pub return_data: ::prost::alloc::vec::Vec<u8>,
+    #[prost(message, optional, tag="15")]
+    pub receipt: ::core::option::Option<TransactionReceipt>,
+    #[prost(message, repeated, tag="16")]
     pub calls: ::prost::alloc::vec::Vec<Call>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct BlockHeader {
+    #[prost(bytes="vec", tag="1")]
+    pub parent_hash: ::prost::alloc::vec::Vec<u8>,
+    /// bytes uncle_hash = 2;
+    /// bytes coinbase = 3;
+    /// bytes state_root = 4;
+    /// bytes transaction_root = 5;
+    #[prost(bytes="vec", tag="6")]
+    pub logs_bloom: ::prost::alloc::vec::Vec<u8>,
+    // optional bytes difficulty = 7;
+    // optional bytes total_difficulty = 8;
+
+    // uint64 number = 9;
+    // uint64 gas_limit = 10;
+    // uint64 gas_used = 11;
+
+    #[prost(uint64, tag="12")]
+    pub timestamp: u64,
+    /// bytes extra_data = 13;
+    /// bytes mix_hash = 14;
+    /// uint64 nonce = 15;
+    #[prost(bytes="vec", tag="16")]
+    pub hash: ::prost::alloc::vec::Vec<u8>,
+    // optional bytes base_fee_per_gas = 17;
+    // bytes withdrawals_root = 18;
+
+    #[prost(message, repeated, tag="19")]
+    pub transactions: ::prost::alloc::vec::Vec<TransactionTrace>,
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum BalanceChangeReason {
+    Unknown = 0,
+    RewardMineUncle = 1,
+    RewardMineBlock = 2,
+    DaoRefundContract = 3,
+    DaoAdjustBalance = 4,
+    Transfer = 5,
+    GenesisBalance = 6,
+    GasBuy = 7,
+    RewardTransactionFee = 8,
+    RewardFeeReset = 14,
+    GasRefund = 9,
+    TouchAccount = 10,
+    SuicideRefund = 11,
+    SuicideWithdraw = 13,
+    CallBalanceOverride = 12,
+    /// / Used on chain(s) where some Ether burning happens
+    Burn = 15,
+    Withdrawal = 16,
+}
+impl BalanceChangeReason {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            BalanceChangeReason::Unknown => "BalanceChangeReasonUnknown",
+            BalanceChangeReason::RewardMineUncle => "RewardMineUncle",
+            BalanceChangeReason::RewardMineBlock => "RewardMineBlock",
+            BalanceChangeReason::DaoRefundContract => "DaoRefundContract",
+            BalanceChangeReason::DaoAdjustBalance => "DaoAdjustBalance",
+            BalanceChangeReason::Transfer => "Transfer",
+            BalanceChangeReason::GenesisBalance => "GenesisBalance",
+            BalanceChangeReason::GasBuy => "GasBuy",
+            BalanceChangeReason::RewardTransactionFee => "RewardTransactionFee",
+            BalanceChangeReason::RewardFeeReset => "RewardFeeReset",
+            BalanceChangeReason::GasRefund => "GasRefund",
+            BalanceChangeReason::TouchAccount => "TouchAccount",
+            BalanceChangeReason::SuicideRefund => "SuicideRefund",
+            BalanceChangeReason::SuicideWithdraw => "SuicideWithdraw",
+            BalanceChangeReason::CallBalanceOverride => "CallBalanceOverride",
+            BalanceChangeReason::Burn => "Burn",
+            BalanceChangeReason::Withdrawal => "Withdrawal",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "BalanceChangeReasonUnknown" => Some(Self::Unknown),
+            "RewardMineUncle" => Some(Self::RewardMineUncle),
+            "RewardMineBlock" => Some(Self::RewardMineBlock),
+            "DaoRefundContract" => Some(Self::DaoRefundContract),
+            "DaoAdjustBalance" => Some(Self::DaoAdjustBalance),
+            "Transfer" => Some(Self::Transfer),
+            "GenesisBalance" => Some(Self::GenesisBalance),
+            "GasBuy" => Some(Self::GasBuy),
+            "RewardTransactionFee" => Some(Self::RewardTransactionFee),
+            "RewardFeeReset" => Some(Self::RewardFeeReset),
+            "GasRefund" => Some(Self::GasRefund),
+            "TouchAccount" => Some(Self::TouchAccount),
+            "SuicideRefund" => Some(Self::SuicideRefund),
+            "SuicideWithdraw" => Some(Self::SuicideWithdraw),
+            "CallBalanceOverride" => Some(Self::CallBalanceOverride),
+            "Burn" => Some(Self::Burn),
+            "Withdrawal" => Some(Self::Withdrawal),
+            _ => None,
+        }
+    }
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum CallType {
+    Unspecified = 0,
+    /// / direct? what's the name for `Call` alone?
+    NormalCall = 1,
+    Callcode = 2,
+    Delegate = 3,
+    Static = 4,
+    /// / create2 ? any other form of calls?
+    Create = 5,
+}
+impl CallType {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            CallType::Unspecified => "Unspecified",
+            CallType::NormalCall => "NormalCall",
+            CallType::Callcode => "Callcode",
+            CallType::Delegate => "Delegate",
+            CallType::Static => "Static",
+            CallType::Create => "Create",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "Unspecified" => Some(Self::Unspecified),
+            "NormalCall" => Some(Self::NormalCall),
+            "Callcode" => Some(Self::Callcode),
+            "Delegate" => Some(Self::Delegate),
+            "Static" => Some(Self::Static),
+            "Create" => Some(Self::Create),
+            _ => None,
+        }
+    }
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum TransactionTraceStatus {
+    Unknown = 0,
+    Succeeded = 1,
+    Failed = 2,
+    Reverted = 3,
+}
+impl TransactionTraceStatus {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            TransactionTraceStatus::Unknown => "Unknown",
+            TransactionTraceStatus::Succeeded => "Succeeded",
+            TransactionTraceStatus::Failed => "Failed",
+            TransactionTraceStatus::Reverted => "Reverted",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "Unknown" => Some(Self::Unknown),
+            "Succeeded" => Some(Self::Succeeded),
+            "Failed" => Some(Self::Failed),
+            "Reverted" => Some(Self::Reverted),
+            _ => None,
+        }
+    }
 }
 // @@protoc_insertion_point(module)
