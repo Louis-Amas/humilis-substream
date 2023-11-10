9e379540454835a92533c50fe6b56f25f655c482
Louis-Amas <amaslouis@protonmail.com>
2023-11-10 15:22:32 +0100

    fix: use BYTEA instead of TEXT

--- scripts/migrations/20231109150017_init.sql
+++ scripts/migrations/20231109150017_init.sql
@@ -1,68 +1,65 @@
-CREATE DOMAIN ethereum_address AS varchar(40);
-CREATE DOMAIN ethereum_hash AS varchar(66);
-
 CREATE TABLE block_header (
-    hash ethereum_hash PRIMARY KEY,
-    parent_hash ethereum_hash,
-    logs_bloom TEXT,
+    hash BYTEA PRIMARY KEY,
+    parent_hash BYTEA,
+    logs_bloom BYTEA,
     timestamp timestamp,
     number INT
 );
 
-CREATE TABLE interface (
-  name TEXT PRIMARY KEY
-);
-
-CREATE TABLE event_type (
-  signature TEXT PRIMARY KEY
-);
-
-CREATE TABLE mapping_event_interface (
-  name TEXT,
-  signature TEXT,
-  FOREIGN KEY (name) REFERENCES interface(name),
-  FOREIGN KEY (signature) REFERENCES event_type(signature),
-
-  PRIMARY KEY (name, signature)
-);
-
-CREATE TABLE contract (
-  address ethereum_address PRIMARY KEY,
-  createdAt time
-);
-
-CREATE TABLE log (
-  block_hash ethereum_hash,
-  address ethereum_address,
-
-  index INTEGER,
-
-  topic0 BYTEA,
-  decoded_topic0 TEXT,
-
-  topic1 BYTEA,
-  decoded_topic1 TEXT,
-
-  topic2 BYTEA,
-  decoded_topic2 TEXT,
-
-  topic3 BYTEA,
-  decoded_topic3 TEXT,
-
-  topic4 BYTEA,
-  decoded_topic4 TEXT,
-
-  data BYTEA,
-  decoded_data TEXT,
-
-  FOREIGN KEY (block_hash) REFERENCES block_header(hash),
-  FOREIGN KEY (address) REFERENCES contract(address),
-
-  signature TEXT,
-  FOREIGN kEY (signature) REFERENCES event_type(signature),
-
-  PRIMARY KEY(address, index)
-);
+-- CREATE TABLE interface (
+--   name TEXT PRIMARY KEY
+-- );
+--
+-- CREATE TABLE event_type (
+--   signature TEXT PRIMARY KEY
+-- );
+--
+-- CREATE TABLE mapping_event_interface (
+--   name TEXT,
+--   signature TEXT,
+--   FOREIGN KEY (name) REFERENCES interface(name),
+--   FOREIGN KEY (signature) REFERENCES event_type(signature),
+--
+--   PRIMARY KEY (name, signature)
+-- );
+--
+-- CREATE TABLE contract (
+--   address ethereum_address PRIMARY KEY,
+--   createdAt time
+-- );
+--
+-- CREATE TABLE log (
+--   block_hash ethereum_hash,
+--   address ethereum_address,
+--
+--   index INTEGER,
+--
+--   topic0 BYTEA,
+--   decoded_topic0 TEXT,
+--
+--   topic1 BYTEA,
+--   decoded_topic1 TEXT,
+--
+--   topic2 BYTEA,
+--   decoded_topic2 TEXT,
+--
+--   topic3 BYTEA,
+--   decoded_topic3 TEXT,
+--
+--   topic4 BYTEA,
+--   decoded_topic4 TEXT,
+--
+--   data BYTEA,
+--   decoded_data TEXT,
+--
+--   FOREIGN KEY (block_hash) REFERENCES block_header(hash),
+--   FOREIGN KEY (address) REFERENCES contract(address),
+--
+--   signature TEXT,
+--   FOREIGN kEY (signature) REFERENCES event_type(signature),
+--
+--   PRIMARY KEY(address, index)
+-- );
 
 CREATE TABLE cursors
 (
